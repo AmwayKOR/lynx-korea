0e301c1bcecd5711d0085f992ab8d0d64767bc15
Parvesh Goyal <parvesh.goyal@nagarro.com>
2017-09-08 16:59:39 +0530

    [AHC-4] - amwayapacstorefrontcomponents extension committed.

---
+++ amwayapacstorefrontcomponents/src/com/amway/apac/storefront/components/constants/AmwayapacstorefrontcomponentsConstants.java
@@ -0,0 +1,28 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2017 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package com.amway.apac.storefront.components.constants;
+
+/**
+ * Global class for all Amwayapacstorefrontcomponents constants. You can add global constants for your extension into this class.
+ */
+public final class AmwayapacstorefrontcomponentsConstants extends GeneratedAmwayapacstorefrontcomponentsConstants
+{
+	public static final String EXTENSIONNAME = "amwayapacstorefrontcomponents";
+
+	private AmwayapacstorefrontcomponentsConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+
+    public static final String PLATFORM_LOGO_CODE = "amwayapacstorefrontcomponentsPlatformLogo";
+}
