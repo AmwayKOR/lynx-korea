b54284bf8ffc2fea2d48846b0b64e73420aa5b96
YongHinWing <Yong.Hin.Wing@amway.com>
2017-10-12 13:34:03 +0800

    Remove unnecessary javascript codes

--- amwayapacstorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/acc.footer.js
+++ amwayapacstorefront/web/webroot/WEB-INF/_ui-src/responsive/lib/ybase-0.1.0/js/acc.footer.js
@@ -5,18 +5,7 @@ ACC.footer = {
 	],
 	
 	bindFooter: function () {
-		function linkToggle () {
-            $(".quick-links-item").click(function () {
-                if($(this).hasClass("switcher")){
-                    $(this).removeClass("switcher");
-                }else{
-                    $(this).addClass("switcher");
-                    $(this).siblings().removeClass("switcher");
-
-                }
-            });
-        }
-        linkToggle();
+		
     }
 
 };
