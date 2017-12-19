d45b2cbbb521d20186cd400d9f43bedafebd923c
Vikrant Ghai <vikrant.ghai@nagarro.com>
2017-12-19 05:37:32 +0530

    [AH2-1070] : Backorder release logic implementation.

--- amwayapaccore/src/com/amway/apac/core/backorder/service/impl/DefaultAmwayApacBackOrderService.java
+++ amwayapaccore/src/com/amway/apac/core/backorder/service/impl/DefaultAmwayApacBackOrderService.java
@@ -3,11 +3,19 @@
  */
 package com.amway.apac.core.backorder.service.impl;
 
+import de.hybris.platform.basecommerce.enums.InStockStatus;
+import de.hybris.platform.core.model.order.OrderEntryModel;
+import de.hybris.platform.ordersplitting.model.ConsignmentEntryModel;
 import de.hybris.platform.ordersplitting.model.ConsignmentModel;
 import de.hybris.platform.ordersplitting.model.ConsignmentProcessModel;
 import de.hybris.platform.ordersplitting.model.StockLevelModel;
 import de.hybris.platform.processengine.BusinessProcessService;
+import de.hybris.platform.processengine.enums.ProcessState;
+import de.hybris.platform.warehousing.inventoryevent.service.InventoryEventService;
+import de.hybris.platform.warehousing.model.AllocationEventModel;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -21,6 +29,7 @@ import org.springframework.beans.factory.annotation.Required;
 import com.amway.apac.core.backorder.service.AmwayApacBackOrderService;
 import com.amway.apac.core.backorder.strategies.AmwayApacBackOrderSelectionStrategy;
 import com.amway.apac.core.model.AmwayBackOrderModel;
+import com.amway.apac.core.stock.strategies.impl.AmwayApacCommerceAvailabilityCalculationStrategy;
 
 
 /**
@@ -34,10 +43,12 @@ public class DefaultAmwayApacBackOrderService implements AmwayApacBackOrderServi
 	private static final Logger LOG = Logger.getLogger(DefaultAmwayApacBackOrderService.class);
 
 	private static final String BACK_ORDER_RELEASE_EVENT_CODE = "WaitForRelease";
+	private static final String CONSIGNMENT_BACKORDER_PROCESS = "consignment-backorder-process";
 
 	private BusinessProcessService businessProcessService;
-
 	private AmwayApacBackOrderSelectionStrategy amwayApacBackOrderSelectionStrategy;
+	private AmwayApacCommerceAvailabilityCalculationStrategy commerceAvailabilityCalculationStrategy;
+	private InventoryEventService inventoryEventService;
 
 	/**
 	 * {@inheritDoc}
@@ -45,17 +56,46 @@ public class DefaultAmwayApacBackOrderService implements AmwayApacBackOrderServi
 	@Override
 	public void releaseBackOrders(final List<AmwayBackOrderModel> amwayBackOrders, final StockLevelModel stockLevel)
 	{
-		if (CollectionUtils.isNotEmpty(amwayBackOrders) && Objects.nonNull(stockLevel))
+		if (CollectionUtils.isNotEmpty(amwayBackOrders) && Objects.nonNull(stockLevel)
+				&& !InStockStatus.BACKORDER.equals(stockLevel.getInStockStatus()))
 		{
-			for (final AmwayBackOrderModel amwayBackOrder : amwayBackOrders)
+			Long available = commerceAvailabilityCalculationStrategy.calculateAvailability(Arrays.asList(stockLevel));
+			if (Objects.nonNull(available))
 			{
-				//TODO checking the stock level vikrant
-				//Trigger consignment process
-				triggerConsignmentProcess(amwayBackOrder.getConsignment());
+				int maxBoReleaseLimit = stockLevel.getMaxBoReleaseLimit();
+				for (final AmwayBackOrderModel amwayBackOrder : amwayBackOrders)
+				{
+					long requestedQty = 0;
+					if (stockLevel.getProduct().equals(amwayBackOrder.getProduct())
+							&& stockLevel.getWarehouse().equals(amwayBackOrder.getWarehouse()))
+					{
+						for (final ConsignmentEntryModel consignmentEntry : amwayBackOrder.getConsignment().getConsignmentEntries())
+						{
+							final Collection<AllocationEventModel> allocationEvents = inventoryEventService
+									.getAllocationEventsForOrderEntry((OrderEntryModel) consignmentEntry.getOrderEntry());
+							requestedQty = allocationEvents
+									.stream()
+									.filter(
+											allocationEvent -> allocationEvent.getConsignmentEntry().getConsignment()
+													.equals(consignmentEntry.getConsignment())).mapToLong(AllocationEventModel::getQuantity)
+									.sum();
+						}
+					}
+					if (requestedQty <= available.longValue() && requestedQty <= maxBoReleaseLimit)
+					{
+						available = Long.valueOf(available.longValue() - requestedQty);
+						maxBoReleaseLimit -= requestedQty;
+						triggerConsignmentProcess(amwayBackOrder.getConsignment());
+					}
+
+					if (available.longValue() == 0 || maxBoReleaseLimit == 0)
+					{
+						break;
+					}
+				}
 			}
 		}
 	}
-
 
 
 	/**
@@ -82,26 +122,30 @@ public class DefaultAmwayApacBackOrderService implements AmwayApacBackOrderServi
 	 *
 	 * @param ConsignmentModel
 	 */
-	private void triggerConsignmentProcess(final ConsignmentModel consModel)
+	private void triggerConsignmentProcess(final ConsignmentModel consignment)
 	{
-		if (Objects.nonNull(consModel))
+		if (Objects.nonNull(consignment))
 		{
 			try
 			{
-				for (final Iterator<ConsignmentProcessModel> iterator = consModel.getConsignmentProcesses().iterator(); iterator
+				for (final Iterator<ConsignmentProcessModel> iterator = consignment.getConsignmentProcesses().iterator(); iterator
 						.hasNext();)
 				{
 					final ConsignmentProcessModel process = iterator.next();
-					final String eventCode = (new StringBuilder(String.valueOf(process.getConsignment().getCode()))).append("_")
-							.append(BACK_ORDER_RELEASE_EVENT_CODE).toString();
-					//Trigger the business process event which release AmwayBackOrder
-					getBusinessProcessService().triggerEvent(eventCode);
-					LOG.info("BackOrder consignment triggered successfully for code : " + consModel.getCode());
+					if (ProcessState.WAITING.equals(process.getProcessState())
+							&& CONSIGNMENT_BACKORDER_PROCESS.equals(process.getProcessDefinitionName()))
+					{
+						final String eventCode = (new StringBuilder(String.valueOf(process.getConsignment().getCode()))).append("_")
+								.append(BACK_ORDER_RELEASE_EVENT_CODE).toString();
+						//Trigger the business process event which release AmwayBackOrder
+						getBusinessProcessService().triggerEvent(eventCode);
+						LOG.info("BackOrder consignment triggered successfully for code : " + consignment.getCode());
+					}
 				}
 			}
 			catch (final Exception e)
 			{
-				logDebugInfo(e, "BackOrder consignment process not triggered successfully for code : " + consModel.getCode());
+				logDebugInfo(e, "BackOrder consignment process not triggered successfully for code : " + consignment.getCode());
 			}
 		}
 
@@ -157,5 +201,26 @@ public class DefaultAmwayApacBackOrderService implements AmwayApacBackOrderServi
 			final AmwayApacBackOrderSelectionStrategy amwayApacBackOrderSelectionStrategy)
 	{
 		this.amwayApacBackOrderSelectionStrategy = amwayApacBackOrderSelectionStrategy;
+	}
+
+	/**
+	 * @param commerceAvailabilityCalculationStrategy
+	 *           the commerceAvailabilityCalculationStrategy to set
+	 */
+	@Required
+	public void setCommerceAvailabilityCalculationStrategy(
+			final AmwayApacCommerceAvailabilityCalculationStrategy commerceAvailabilityCalculationStrategy)
+	{
+		this.commerceAvailabilityCalculationStrategy = commerceAvailabilityCalculationStrategy;
+	}
+
+	/**
+	 * @param inventoryEventService
+	 *           the inventoryEventService to set
+	 */
+	@Required
+	public void setInventoryEventService(final InventoryEventService inventoryEventService)
+	{
+		this.inventoryEventService = inventoryEventService;
 	}
 }
