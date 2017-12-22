0e398c112947b88e8114f0f1254249f4efd1dd91
NAGARROdeepankarshukla <deepankar.shukla@nagarro.com>
2017-12-22 13:24:58 +0530

    commit error resolution

--- amwayapacbackoffice/backoffice/src/com/amway/apac/backoffice/renderers/BackOrderShippingAddressRenderer.java
+++ amwayapacbackoffice/backoffice/src/com/amway/apac/backoffice/renderers/BackOrderShippingAddressRenderer.java
@@ -56,31 +56,37 @@ public class BackOrderShippingAddressRenderer implements WidgetComponentRenderer
 	{
 		final Map<String, Object> attributeMap = new LinkedHashMap();
 		final ConsignmentModel consignment = backOrderModel.getConsignment();
-
-		final AddressModel shippingaddress = consignment.getShippingAddress();
-		final String itemsLabel1 = Labels.getLabel("amway.backorder.section.shippingaddress.field1");
-		final String itemsvalue1 = shippingaddress.getLine1() + " " + shippingaddress.getBuilding() + " "
-				+ shippingaddress.getStreetnumber();
-		attributeMap.put(itemsLabel1, itemsvalue1.replace("null", ""));
-		final String itemsLabel2 = Labels.getLabel("amway.backorder.section.shippingaddress.field2");
-		final String itemsvalue2 = shippingaddress.getLine2() + " " + shippingaddress.getAppartment() + " "
-				+ shippingaddress.getStreetname();
-		attributeMap.put(itemsLabel2, itemsvalue2.replace("null", ""));
-		final String itemsLabel3 = Labels.getLabel("amway.backorder.section.shippingaddress.field3");
-		final String itemsvalue3 = shippingaddress.getLine3() + " " + shippingaddress.getLandMark();
-		attributeMap.put(itemsLabel3, itemsvalue3.replace("null", ""));
-		final String itemsLabel4 = Labels.getLabel("amway.backorder.section.shippingaddress.postalcode");
-		final String itemsvalue4 = shippingaddress.getPostalcode();
-		attributeMap.put(itemsLabel4, itemsvalue4.replace("null", ""));
-		final String itemsLabel5 = Labels.getLabel("amway.backorder.section.shippingaddress.city");
-		final String itemsvalue5 = shippingaddress.getDistrict() + "" + shippingaddress.getTown();
-		attributeMap.put(itemsLabel5, itemsvalue5.replace("null", ""));
-		final String itemsLabel6 = Labels.getLabel("amway.backorder.section.shippingaddress.stateprovince");
-		final String itemsvalue6 = shippingaddress.getRegion().getName() + " " + shippingaddress.getRegion().getIsocode();
-		attributeMap.put(itemsLabel6, itemsvalue6.replace("null", ""));
-		final String itemsLabel7 = Labels.getLabel("amway.backorder.section.shippingaddress.country");
-		final String itemsvalue7 = shippingaddress.getCountry().getName() + " " + shippingaddress.getCountry().getCountryCode();
-		attributeMap.put(itemsLabel7, itemsvalue7.replace("null", ""));
+		if (null != consignment && null != consignment.getShippingAddress())
+		{
+			final AddressModel shippingaddress = consignment.getShippingAddress();
+			final String itemsLabel1 = Labels.getLabel("amway.backorder.section.shippingaddress.field1");
+			final String itemsvalue1 = shippingaddress.getLine1() + " " + shippingaddress.getBuilding() + " "
+					+ shippingaddress.getStreetnumber();
+			attributeMap.put(itemsLabel1, itemsvalue1.replace("null", ""));
+			final String itemsLabel2 = Labels.getLabel("amway.backorder.section.shippingaddress.field2");
+			final String itemsvalue2 = shippingaddress.getLine2() + " " + shippingaddress.getAppartment() + " "
+					+ shippingaddress.getStreetname();
+			attributeMap.put(itemsLabel2, itemsvalue2.replace("null", ""));
+			final String itemsLabel3 = Labels.getLabel("amway.backorder.section.shippingaddress.field3");
+			final String itemsvalue3 = shippingaddress.getLine3() + " " + shippingaddress.getLandMark();
+			attributeMap.put(itemsLabel3, itemsvalue3.replace("null", ""));
+			final String itemsLabel4 = Labels.getLabel("amway.backorder.section.shippingaddress.postalcode");
+			final String itemsvalue4 = shippingaddress.getPostalcode();
+			attributeMap.put(itemsLabel4, itemsvalue4.replace("null", ""));
+			final String itemsLabel5 = Labels.getLabel("amway.backorder.section.shippingaddress.city");
+			final String itemsvalue5 = shippingaddress.getDistrict() + "" + shippingaddress.getTown();
+			attributeMap.put(itemsLabel5, itemsvalue5.replace("null", ""));
+			final String itemsLabel6 = Labels.getLabel("amway.backorder.section.shippingaddress.stateprovince");
+			final String itemsvalue6 = shippingaddress.getRegion().getName() + " " + shippingaddress.getRegion().getIsocode();
+			attributeMap.put(itemsLabel6, itemsvalue6.replace("null", ""));
+			final String itemsLabel7 = Labels.getLabel("amway.backorder.section.shippingaddress.country");
+			final String itemsvalue7 = shippingaddress.getCountry().getName() + " " + shippingaddress.getCountry().getCountryCode();
+			attributeMap.put(itemsLabel7, itemsvalue7.replace("null", ""));
+		}
+		else
+		{
+			LOG.info("No shipping Address found for the backorder " + backOrderModel.getConsignment().getCode());
+		}
 
 		ApacAttributeWithLabelRendererUtil.createAttributeWithLabel(parent, attributeMap, 3);
 
