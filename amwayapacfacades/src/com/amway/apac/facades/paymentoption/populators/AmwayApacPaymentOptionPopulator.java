41f4a9592a4749a912df00d221882564995999d5
Ashish Sabal <ashish.sabal@nagarro.com>
2018-01-03 16:03:09 +0530

    [AHC-262] : Payment option populator import

---
+++ amwayapacfacades/src/com/amway/apac/facades/paymentoption/populators/AmwayApacPaymentOptionPopulator.java
@@ -0,0 +1,100 @@
+/**
+ *
+ */
+package com.amway.apac.facades.paymentoption.populators;
+
+import static de.hybris.platform.servicelayer.util.ServicesUtil.validateParameterNotNullStandardMessage;
+
+import de.hybris.platform.converters.Populator;
+import de.hybris.platform.core.HybrisEnumValue;
+import de.hybris.platform.core.model.product.ProductModel;
+import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+import de.hybris.platform.servicelayer.dto.converter.Converter;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Required;
+
+import com.amway.apac.core.enums.PaymentType;
+import com.amway.apac.core.model.AmwayApacVariantProductModel;
+import com.amway.apac.core.model.AmwayPaymentOptionModel;
+import com.amway.apacfacades.data.EnumData;
+import com.amway.apacfacades.payment.data.AmwayPaymentOptionData;
+
+
+public class AmwayApacPaymentOptionPopulator implements Populator<AmwayPaymentOptionModel, AmwayPaymentOptionData>
+{
+	private Converter<HybrisEnumValue, EnumData> enumConverter;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void populate(final AmwayPaymentOptionModel source, final AmwayPaymentOptionData target) throws ConversionException
+	{
+		validateParameterNotNullStandardMessage("target", source);
+		validateParameterNotNullStandardMessage("source", target);
+
+
+		target.setAliasCode(source.getAliasCode());
+
+		if (PaymentType.PIF.equals(source.getType()))
+		{
+			target.setTitle(getProductName(source));
+		}
+		else
+		{
+			if (StringUtils.isNotBlank(source.getTitle()))
+			{
+				target.setTitle(source.getTitle());
+			}
+			else
+			{
+				if (source.getProduct() != null)
+				{
+					target.setTitle(getProductName(source));
+				}
+			}
+		}
+
+		target.setPaymentType(source.getType());
+		target.setPaymentTypeData(getEnumConverter().convert(source.getType()));
+	}
+
+	/**
+	 * @param source
+	 * @return
+	 */
+	private String getProductName(final AmwayPaymentOptionModel paymentOption)
+	{
+		final ProductModel product = paymentOption.getProduct();
+		String productName = StringUtils.EMPTY;
+		if (null != product)
+		{
+			productName = product.getName();
+		}
+		if (StringUtils.isEmpty(productName) && product instanceof AmwayApacVariantProductModel
+				&& (null != ((AmwayApacVariantProductModel) product).getBaseProduct()))
+		{
+			productName = ((AmwayApacVariantProductModel) product).getBaseProduct().getName();
+		}
+		return productName;
+	}
+
+	/**
+	 * @return the enumConverter
+	 */
+	public Converter<HybrisEnumValue, EnumData> getEnumConverter()
+	{
+		return enumConverter;
+	}
+
+	/**
+	 * @param enumConverter
+	 *           the enumConverter to set
+	 */
+	@Required
+	public void setEnumConverter(final Converter<HybrisEnumValue, EnumData> enumConverter)
+	{
+		this.enumConverter = enumConverter;
+	}
+}
---
+++ amwayapacfacades/src/com/amway/apac/facades/populators/AmwayApacEnumDataPopulator.java
@@ -0,0 +1,45 @@
+/**
+ *
+ */
+package com.amway.apac.facades.populators;
+
+import de.hybris.platform.converters.Populator;
+import de.hybris.platform.core.HybrisEnumValue;
+import de.hybris.platform.enumeration.EnumerationService;
+import de.hybris.platform.servicelayer.dto.converter.ConversionException;
+
+import org.springframework.beans.factory.annotation.Required;
+
+import com.amway.apacfacades.data.EnumData;
+
+
+/**
+ *
+ */
+public class AmwayApacEnumDataPopulator implements Populator<HybrisEnumValue, EnumData>
+{
+	private EnumerationService enumerationService;
+
+	/*
+	 * (non-Javadoc)
+	 *
+	 * @see de.hybris.platform.converters.Populator#populate(java.lang.Object, java.lang.Object)
+	 */
+	@Override
+	public void populate(final HybrisEnumValue source, final EnumData target) throws ConversionException
+	{
+		target.setCode(source.getCode());
+		target.setLabel(enumerationService.getEnumerationName(source));
+	}
+
+	/**
+	 * @param enumerationService
+	 *           the enumerationService to set
+	 */
+	@Required
+	public void setEnumerationService(final EnumerationService enumerationService)
+	{
+		this.enumerationService = enumerationService;
+	}
+
+}
