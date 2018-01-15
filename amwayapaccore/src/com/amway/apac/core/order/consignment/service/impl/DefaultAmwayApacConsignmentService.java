29a86eaae17bd6f40810bc2c82c9da7c7fc2e5db
Navish Sharma <navish.sharma@nagarro.com>
2018-01-15 17:38:15 +0530

    AH2-1070 AH2-928 Junit Test case for DefaultAmwayApacConnsignmentService

--- amwayapaccore/src/com/amway/apac/core/order/consignment/service/impl/DefaultAmwayApacConsignmentService.java
+++ amwayapaccore/src/com/amway/apac/core/order/consignment/service/impl/DefaultAmwayApacConsignmentService.java
@@ -15,6 +15,7 @@ import de.hybris.platform.servicelayer.model.ModelService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.commons.collections.CollectionUtils;
@@ -51,35 +52,39 @@ public class DefaultAmwayApacConsignmentService extends DefaultAmwayConsignmentS
 	public List<ConsignmentModel> createConsignments(final AbstractOrderModel order) throws ConsignmentCreationException
 	{
 		final List<ConsignmentModel> allConsignments = new ArrayList<>();
-		final AtomicLong index = new AtomicLong();
-		final List<AbstractOrderEntryModel> remainingEntries = new ArrayList<>();
-		for (final AbstractOrderEntryModel entry : order.getEntries())
+		if (Objects.nonNull(order))
 		{
-			if (entry.getDispositionCode().equals(InStockStatus.BACKORDER))
+			final AtomicLong index = new AtomicLong();
+			final List<AbstractOrderEntryModel> remainingEntries = new ArrayList<>();
+			for (final AbstractOrderEntryModel entry : order.getEntries())
 			{
-				final ConsignmentModel consignment = createConsignment(order,
-						PREFIX_CODE + order.getCode() + '_' + index.getAndIncrement(), Arrays.asList(entry));
-				if (consignment != null)
+				if (null != entry.getDispositionCode() && entry.getDispositionCode().equals(InStockStatus.BACKORDER))
 				{
-					final String consignmentBackOrderProcessDefName = "consignment-backorder-process";
-					final String consignmentBackOrderProcessCode = new StringBuilder(consignmentBackOrderProcessDefName)
-							.append(PROCESS_CODE_SEPARATOR).append(consignment.getCode()).append(PROCESS_CODE_SEPARATOR)
-							.append(System.currentTimeMillis()).toString();
-					final ConsignmentProcessModel consignmentBackOrderProcess = businessProcessService
-							.createProcess(consignmentBackOrderProcessCode, consignmentBackOrderProcessDefName);
-					LOG.info(String.format("Created consignment with code : [%s] for order with code : [%s] ", consignment.getCode(),
-							consignment.getOrder().getCode()));
-					consignmentBackOrderProcess.setConsignment(consignment);
-					getModelService().save(consignmentBackOrderProcess);
-					businessProcessService.startProcess(consignmentBackOrderProcess);
+					final ConsignmentModel consignment = createConsignment(order,
+							PREFIX_CODE + order.getCode() + '_' + index.getAndIncrement(), Arrays.asList(entry));
+					if (consignment != null)
+					{
+						final String consignmentBackOrderProcessDefName = "consignment-backorder-process";
+						final String consignmentBackOrderProcessCode = new StringBuilder(consignmentBackOrderProcessDefName)
+								.append(PROCESS_CODE_SEPARATOR).append(consignment.getCode()).append(PROCESS_CODE_SEPARATOR)
+								.append(System.currentTimeMillis()).toString();
+						final ConsignmentProcessModel consignmentBackOrderProcess = businessProcessService
+								.createProcess(consignmentBackOrderProcessCode, consignmentBackOrderProcessDefName);
+						LOG.info(String.format("Created consignment with code : [%s] for order with code : [%s] ",
+								consignment.getCode(), consignment.getOrder().getCode()));
+						consignmentBackOrderProcess.setConsignment(consignment);
+						getModelService().save(consignmentBackOrderProcess);
+						businessProcessService.startProcess(consignmentBackOrderProcess);
+						allConsignments.add(consignment);
+					}
+				}
+				else
+				{
+					remainingEntries.add(entry);
 				}
 			}
-			else
-			{
-				remainingEntries.add(entry);
-			}
+			createConsignmentForRemainingEntries(order, remainingEntries, allConsignments);
 		}
-		createConsignmentForRemainingEntries(order, remainingEntries, allConsignments);
 		return allConsignments;
 	}
 
